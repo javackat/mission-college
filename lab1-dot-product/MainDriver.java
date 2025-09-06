68c90a4f100125451bbf06e6b9a8f3b56398f8bc
javackat <javac.kat@gmail.com>
2025-09-06 00:52:39 -0700

    Create MainDriver.java

---
+++ lab1-dot-product/MainDriver.java
@@ -0,0 +1,29 @@
+//TIP To <b45>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import DotProduct.*;
+//import Calculator.*;
+//import Example1.*;
+//import Scan.*;
+//import java.util.Stack;
+
+public class MainDriver {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        MainDriver driver = new MainDriver();
+
+        System.out.printf("Dot Product! \n");
+
+        DotProduct dt = new DotProduct( 5);
+        dt.print();
+
+
+
+//        Calculator calc = new Calculator( );
+//        calc.input();
+
+
+    }
+}
